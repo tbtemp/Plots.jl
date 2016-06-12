0d83d918864346d45b35382a14a4e23a5f3679e1
Thomas Breloff <tom@breloff.com>
2016-06-12 12:34:41 -0400

    big cleanup: changed camelcase to underscores; removed supportedAxes; changed pyplot methods to be py_ which matches gr_/plotly_/etc convention
    Former-commit-id: 02d893b316b7bd0625d059ff9b62ebe16393de18

---
+++ src/Plots.jl
@@ -0,0 +1,248 @@
+
+ __precompile__()
+
+module Plots
+
+using Compat
+using Reexport
+@reexport using Colors
+# using Requires
+using FixedSizeArrays
+@reexport using RecipesBase
+using Base.Meta
+
+export
+    AbstractPlot,
+    Plot,
+    Subplot,
+    AbstractLayout,
+    GridLayout,
+    grid,
+    EmptyLayout,
+    @layout,
+    AVec,
+    AMat,
+    KW,
+
+    wrap,
+    set_theme,
+    add_theme,
+
+    plot,
+    plot!,
+
+    current,
+    default,
+    with,
+
+    @userplot,
+    @shorthands,
+
+    pie,
+    pie!,
+    plot3d,
+    plot3d!,
+
+    title!,
+    xlabel!,
+    ylabel!,
+    xlims!,
+    ylims!,
+    zlims!,
+    xticks!,
+    yticks!,
+    annotate!,
+    xflip!,
+    yflip!,
+    xaxis!,
+    yaxis!,
+
+    savefig,
+    png,
+    gui,
+
+    backend,
+    backends,
+    backend_name,
+    aliases,
+    dataframes,
+
+    Shape,
+    text,
+    font,
+    Axis,
+    stroke,
+    brush,
+    Surface,
+    OHLC,
+    arrow,
+
+    colorscheme,
+    ColorScheme,
+    ColorGradient,
+    ColorVector,
+    ColorWrapper,
+    ColorFunction,
+    ColorZFunction,
+    getColor,
+    getColorZ,
+
+    debugplots,
+
+    supported_args,
+    supported_types,
+    supported_styles,
+    supported_markers,
+    is_subplot_supported,
+
+    Animation,
+    frame,
+    gif,
+    @animate,
+    @gif,
+
+    PlotRecipe,
+    spy,
+    arcdiagram,
+    chorddiagram,
+
+    test_examples,
+
+    translate,
+    translate!,
+    rotate,
+    rotate!,
+    center,
+    P2,
+    P3,
+    BezierCurve,
+    curve_points,
+    directed_curve
+
+# ---------------------------------------------------------
+
+import Measures
+import Measures: Length, AbsoluteLength, Measure, BoundingBox, mm, cm, inch, pt, width, height
+typealias BBox Measures.Absolute2DBox
+export BBox, BoundingBox, mm, cm, inch, pt, px, pct
+
+# ---------------------------------------------------------
+
+include("types.jl")
+include("utils.jl")
+include("colors.jl")
+include("components.jl")
+include("axes.jl")
+include("backends.jl")
+include("args.jl")
+include("themes.jl")
+include("plot.jl")
+include("series_args.jl")
+include("series_new.jl")
+include("layouts.jl")
+include("subplots.jl")
+include("recipes.jl")
+include("animation.jl")
+include("output.jl")
+include("examples.jl")
+include("arg_desc.jl")
+
+
+# ---------------------------------------------------------
+
+# define and export shorthand plotting method definitions
+macro shorthands(funcname::Symbol)
+    funcname2 = Symbol(funcname, "!")
+    esc(quote
+        export $funcname, $funcname2
+        $funcname(args...; kw...) = plot(args...; kw..., seriestype = $(quot(funcname)))
+        $funcname2(args...; kw...) = plot!(args...; kw..., seriestype = $(quot(funcname)))
+    end)
+end
+
+@shorthands scatter
+@shorthands bar
+@shorthands barh
+@shorthands histogram
+@shorthands histogram2d
+@shorthands density
+@shorthands heatmap
+@shorthands hexbin
+@shorthands sticks
+@shorthands hline
+@shorthands vline
+@shorthands ohlc
+@shorthands contour
+@shorthands contour3d
+@shorthands surface
+@shorthands wireframe
+@shorthands path3d
+@shorthands scatter3d
+@shorthands boxplot
+@shorthands violin
+@shorthands quiver
+
+pie(args...; kw...)        = plot(args...; kw...,  seriestype = :pie, aspect_ratio = :equal, grid=false, xticks=nothing, yticks=nothing)
+pie!(args...; kw...)       = plot!(args...; kw..., seriestype = :pie, aspect_ratio = :equal, grid=false, xticks=nothing, yticks=nothing)
+plot3d(args...; kw...)     = plot(args...; kw...,  seriestype = :path3d)
+plot3d!(args...; kw...)    = plot!(args...; kw..., seriestype = :path3d)
+
+
+title!(s::AbstractString; kw...)                 = plot!(; title = s, kw...)
+xlabel!(s::AbstractString; kw...)                = plot!(; xlabel = s, kw...)
+ylabel!(s::AbstractString; kw...)                = plot!(; ylabel = s, kw...)
+xlims!{T<:Real,S<:Real}(lims::Tuple{T,S}; kw...) = plot!(; xlims = lims, kw...)
+ylims!{T<:Real,S<:Real}(lims::Tuple{T,S}; kw...) = plot!(; ylims = lims, kw...)
+zlims!{T<:Real,S<:Real}(lims::Tuple{T,S}; kw...) = plot!(; zlims = lims, kw...)
+xlims!(xmin::Real, xmax::Real; kw...)                     = plot!(; xlims = (xmin,xmax), kw...)
+ylims!(ymin::Real, ymax::Real; kw...)                     = plot!(; ylims = (ymin,ymax), kw...)
+zlims!(zmin::Real, zmax::Real; kw...)                     = plot!(; zlims = (zmin,zmax), kw...)
+xticks!{T<:Real}(v::AVec{T}; kw...)                       = plot!(; xticks = v, kw...)
+yticks!{T<:Real}(v::AVec{T}; kw...)                       = plot!(; yticks = v, kw...)
+xticks!{T<:Real,S<:AbstractString}(
+              ticks::AVec{T}, labels::AVec{S}; kw...)     = plot!(; xticks = (ticks,labels), kw...)
+yticks!{T<:Real,S<:AbstractString}(
+              ticks::AVec{T}, labels::AVec{S}; kw...)     = plot!(; yticks = (ticks,labels), kw...)
+annotate!(anns...; kw...)                                 = plot!(; annotation = anns, kw...)
+annotate!{T<:Tuple}(anns::AVec{T}; kw...)                 = plot!(; annotation = anns, kw...)
+xflip!(flip::Bool = true; kw...)                          = plot!(; xflip = flip, kw...)
+yflip!(flip::Bool = true; kw...)                          = plot!(; yflip = flip, kw...)
+xaxis!(args...; kw...)                                    = plot!(; xaxis = args, kw...)
+yaxis!(args...; kw...)                                    = plot!(; yaxis = args, kw...)
+
+title!(plt::Plot, s::AbstractString; kw...)                  = plot!(plt; title = s, kw...)
+xlabel!(plt::Plot, s::AbstractString; kw...)                 = plot!(plt; xlabel = s, kw...)
+ylabel!(plt::Plot, s::AbstractString; kw...)                 = plot!(plt; ylabel = s, kw...)
+xlims!{T<:Real,S<:Real}(plt::Plot, lims::Tuple{T,S}; kw...)  = plot!(plt; xlims = lims, kw...)
+ylims!{T<:Real,S<:Real}(plt::Plot, lims::Tuple{T,S}; kw...)  = plot!(plt; ylims = lims, kw...)
+zlims!{T<:Real,S<:Real}(plt::Plot, lims::Tuple{T,S}; kw...)  = plot!(plt; zlims = lims, kw...)
+xlims!(plt::Plot, xmin::Real, xmax::Real; kw...)                      = plot!(plt; xlims = (xmin,xmax), kw...)
+ylims!(plt::Plot, ymin::Real, ymax::Real; kw...)                      = plot!(plt; ylims = (ymin,ymax), kw...)
+zlims!(plt::Plot, zmin::Real, zmax::Real; kw...)                      = plot!(plt; zlims = (zmin,zmax), kw...)
+xticks!{T<:Real}(plt::Plot, ticks::AVec{T}; kw...)                    = plot!(plt; xticks = ticks, kw...)
+yticks!{T<:Real}(plt::Plot, ticks::AVec{T}; kw...)                    = plot!(plt; yticks = ticks, kw...)
+xticks!{T<:Real,S<:AbstractString}(plt::Plot,
+                          ticks::AVec{T}, labels::AVec{S}; kw...)     = plot!(plt; xticks = (ticks,labels), kw...)
+yticks!{T<:Real,S<:AbstractString}(plt::Plot,
+                          ticks::AVec{T}, labels::AVec{S}; kw...)     = plot!(plt; yticks = (ticks,labels), kw...)
+annotate!(plt::Plot, anns...; kw...)                                  = plot!(plt; annotation = anns, kw...)
+annotate!{T<:Tuple}(plt::Plot, anns::AVec{T}; kw...)                  = plot!(plt; annotation = anns, kw...)
+xflip!(plt::Plot, flip::Bool = true; kw...)                           = plot!(plt; xflip = flip, kw...)
+yflip!(plt::Plot, flip::Bool = true; kw...)                           = plot!(plt; yflip = flip, kw...)
+xaxis!(plt::Plot, args...; kw...)                                     = plot!(plt; xaxis = args, kw...)
+yaxis!(plt::Plot, args...; kw...)                                     = plot!(plt; yaxis = args, kw...)
+
+
+
+# ---------------------------------------------------------
+
+const CURRENT_BACKEND = CurrentBackend(:none)
+
+function __init__()
+    setup_ijulia()
+    setup_atom()
+end
+
+# ---------------------------------------------------------
+
+end # module
